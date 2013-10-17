ea55a3188ccf8b18387d460448321b807f0f9035
NeoHuang <superneohuang@gmail.com>
2013-10-17 23:58:16 +0200

    restaurant list

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Yammi::Application.routes.draw do
 
   resources :cities
 
+  root :to => "welcome#index"
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
