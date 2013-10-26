4166dc7a678373fcc7baaa30dabe064b9a6efda7
kevinzcf <kevinzcf@gmail.com>
2013-10-26 21:55:17 +0200

    add validates

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,7 @@ class Restaurant < ActiveRecord::Base
   belongs_to :city
   has_many :dishes
   has_and_belongs_to_many :restaurant_categories
+  
+  validates_presence_of :name, :street, :zip_code
+  validates_uniqueness_of :name, :scope => :zip_code
 end
